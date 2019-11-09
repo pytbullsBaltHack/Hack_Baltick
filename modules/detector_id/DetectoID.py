e572b3ca6d5271a14a3217a263d3b31cca18d9df
Alexey_B <ph.begaev@gmail.com>
2019-11-09 16:14:46 +0300

    add DetectorID

--- modules/detector_id/DetectoID.py
+++ modules/detector_id/DetectoID.py
@@ -2,3 +2,26 @@
 @author Alexey_B
 @brief Class for calculate ID
 """
+
+
+class FaceId(object):
+    id = []
+
+    def __init__(self, id):
+        self.id = id
+        return
+
+
+class DetectorId(object):
+
+    def __init__(self):
+        # Инициализация детектора...
+        pass
+
+    def init(self, params):
+        pass
+
+    # frame: cv2::Mat
+    def predict(self, frame, rois):
+        ids = [[1, 2, 3]]
+        return ids
