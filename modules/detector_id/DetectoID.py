5b38d307e208981da8d209b5ad7751bf86755916
Alexey_B <ph.begaev@gmail.com>
2019-11-09 20:11:13 +0300

    add predict for DetectorId

--- modules/detector_id/DetectoID.py
+++ modules/detector_id/DetectoID.py
@@ -1,3 +1,14 @@
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+import numpy as np
+import pandas as pd
+import os
+
+workers = 0 if os.name == 'nt' else 4
+
+
 """
 @author Alexey_B
 @brief Class for calculate ID
@@ -14,16 +25,14 @@ class FaceId(object):
 
 class DetectorId(object):
 
-    def __init__(self):
-        # Инициализация детектора...
-        pass
+    def __init__(self, param):
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.model = InceptionResnetV1(pretrained='vggface2').eval().to(self.device)
 
-    def init(self, params):
-        pass
 
     # frame: cv2::Mat
     def predict(self, frame, rois):
-        ids = list()
-        id = FaceId([i for i in range(50)])
-        ids.append(id)
+        aligned = self.prepare_data(frame, rois)
+        embeddings = self.model(aligned).detach().cpu()
+        ids = self.post_data(embeddings)
         return ids
